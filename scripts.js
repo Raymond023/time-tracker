1e676de2435b5630b0a942e8fba02db7dd3dcc39
Raymond023 <raymondraphael11@gmail.com>
2024-12-04 20:28:58 +0100

    added web functions to the site

--- scripts.js
+++ scripts.js
@@ -1,2 +1,31 @@
 'use strict';
 
+const weekly = $('.weekly');
+const activity = $('.activity');
+const daily = $('.daily');
+const monthly = $('.monthly');
+const list = $('li');
+
+
+$(list[0]).click(function () {
+    daily.addClass("selected");
+    weekly.removeClass("selected");
+    monthly.removeClass("selected");
+});
+
+$(list[1]).click(function () {
+    daily.removeClass("selected");
+    weekly.addClass("selected");
+    monthly.removeClass("selected");
+});
+
+$(list[2]).click(function () {
+    daily.removeClass("selected");
+    weekly.removeClass("selected");
+    monthly.addClass("selected");
+});
+
+list.click(function () {
+    list.removeClass("active");
+    $(this).addClass("active");
+});
